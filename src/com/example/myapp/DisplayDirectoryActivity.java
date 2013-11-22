968ee67ac1ce0cd35c86482adebb3f2329684d2f
abnegate <jake_barnby@hotmail.com>
2013-11-23 08:43:27 +1300

    Issues with search function, leaving for now and working on displaying icons in list view.

--- src/com/example/myapp/DisplayDirectoryActivity.java
+++ src/com/example/myapp/DisplayDirectoryActivity.java
@@ -19,11 +19,12 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class DisplayDirectoryActivity extends Activity {
-	
-	List<Map<String, String>> currentFileList = new ArrayList<Map<String,String>>();
-	
+
+	List<Map<String, String>> currentFileList = new ArrayList<Map<String, String>>();
+
 	String path;
-	
+	String query;
+
 	ListView lv;
 	SimpleAdapter simpleAdpt;
 
@@ -33,103 +34,126 @@ public class DisplayDirectoryActivity extends Activity {
 		setContentView(R.layout.display_directory_activity);
 		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		//Receive current directory path
+
+		// Receive current directory path
 		String currentPath = (String) getIntent().getCharSequenceExtra("currentPath");
 		path = currentPath;
-		
-		//Add all directories on external SD card to List->Map for display in ListView
+
+		// Add all directories on external SD card to List->Map for display in
+		// ListView
 		populateFiles();
-		
-		//Create ListView attached to ListView in xml definition
+
+		// Create ListView attached to ListView in xml definition
 		lv = (ListView) findViewById(R.id.listView);
-		
-		//Create a new adapter with the files needed to be listed
-		simpleAdpt = new SimpleAdapter(this, currentFileList, android.R.layout.simple_list_item_1, new String[] {"file"}, new int[] {android.R.id.text1});
-		//Set the new adapter to the ListView
+
+		// Create a new adapter with the files needed to be listed
+		simpleAdpt = new SimpleAdapter(this.getBaseContext(), currentFileList, R.layout.list_view, new String[] { "file", "img" }, new int[] { R.id.file, R.id.img });
+		// Set the new adapter to the ListView
 		lv.setAdapter(simpleAdpt);
-		
-		//Set up the ListView to accept clicking on items
+
+		// Set up the ListView to accept clicking on items
 		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			
+
 			public void onItemClick(AdapterView<?> parentAdapter, View view, int position, long id) {
-				//View is a text view so it can be cast
+				// View is a text view so it can be cast
 				TextView clickedItem = (TextView) view;
-				
-				//Update path and listview to new directory based on what was clicked
+
+				// Update path and listview to new directory based on what was
+				// clicked
 				path += "/" + clickedItem.getText();
-				
+
 				File currentFile = new File(path);
-				//Create a new file from the updated path and check if it is a directory or file
+				// Create a new file from the updated path and check if it is a
+				// directory or file
 				if (currentFile.isDirectory()) {
 					populateFiles();
 					simpleAdpt.notifyDataSetChanged();
-				}
+				} 
+				
 				else if (currentFile.isFile()) {
-					//Create new intent and set it 
-	                Intent intent = new Intent();
-	                intent.setAction(android.content.Intent.ACTION_VIEW);
-	               
-	                //Get extension type of file
-	                MimeTypeMap mime = MimeTypeMap.getSingleton();
-	                String ext=currentFile.getName().substring(currentFile.getName().indexOf(".")+1);
-	                String type = mime.getMimeTypeFromExtension(ext);
-	                //Give intent the filename and extension
-	                intent.setDataAndType(Uri.fromFile(currentFile),type);
-	                //Set path back to parent of file opened
+					// Create new intent and set it
+					Intent intent = new Intent();
+					intent.setAction(android.content.Intent.ACTION_VIEW);
+
+					// Get extension type of file
+					MimeTypeMap mime = MimeTypeMap.getSingleton();
+					String ext = currentFile.getName().substring(currentFile.getName().indexOf(".") + 1);
+					String type = mime.getMimeTypeFromExtension(ext);
+					
+					// Give intent the filename and extension
+					intent.setDataAndType(Uri.fromFile(currentFile), type);
+					
+					// Set path back to parent of file opened
 					File currentDirectory = new File(path);
 					path = currentDirectory.getParent();
-	                //Start the new activity, launching the file with defualt application
-	                startActivity(intent);
+					
+					// Start the new activity, launching the file with defualt application
+					startActivity(intent);
 				}
 			}
 		});
 	}
-	 @Override
-	 public boolean onCreateOptionsMenu(Menu menu) {
-		 // Inflate the menu; this adds items to the action bar if it is present.
-		 getMenuInflater().inflate(R.menu.main_activity_actions, menu);
-		 return true;
-	 }
-	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+		return true;
+	}
+
 	public void populateFiles() {
-		//Clear list for updating
+		// Clear list for updating
 		currentFileList.clear();
-		//Create a file from the current path
-		File root = new File(path);
-		//Array of the files within the current directory
-		String[] subFiles = root.list();
-	
-		//Add each file in the current directory to currentFileList
-		for (int i = 0; i < subFiles.length-1; i++) {
-			currentFileList.add(createEntry("file", subFiles[i]));
+		// Create a file from the current path
+		File path = new File(this.path);
+		// Array of the files within the current directory
+		String[] subFiles = path.list();
+
+		// Add each file in the current directory to currentFileList
+		for (int i = 0; i < subFiles.length; i++) {
+			File current = new File(subFiles[i]);
+			if (current.isFile())
+				currentFileList.add(createEntry("file", subFiles[i], String.valueOf(android.R.drawable.btn_default)));
+			else if (current.isDirectory())
+				currentFileList.add(createEntry("directory", subFiles[i], String.valueOf(android.R.drawable.arrow_down_float)));
 		}
 	}
 	
-	//Create a map of key to filename for adding to the currentFileList
-	private HashMap<String, String> createEntry(String key, String name) {
-		HashMap<String, String> entry = new HashMap<String, String>();
-		entry.put(key, name);
-		
-		return entry;
-		
+/*	public void populateSearch(String query) {
+		currentFileList.clear();
+		File root = new File(Environment.getRootDirectory().getPath());
+		String[] subFiles = root.list();
+		for (int i = 0; i < subFiles.length; i++) {
+			if (query.equals(subFiles[i])) {
+				currentFileList.add(createEntry("file", subFiles[i]));
+			}
+		}
 	}
-	
-	//Override back button to move up one level in the filesystem on press
+*/
+
+	// Create a map of key to filename for adding to the currentFileList
+	private HashMap<String, String> createEntry(String type, String name, String imgPath) {
+		HashMap<String, String> entry = new HashMap<String, String>();
+		entry.put("file", name);
+		entry.put("img", imgPath);
+		return entry;
+
+	}
+
+	// Override back button to move up one level in the filesystem on press
 	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event)  {
-		if (path.length() > 1) {
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (path.length() > 5) {
 			if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
 				File currentDirectory = new File(path);
 				path = currentDirectory.getParent();
 				populateFiles();
 				simpleAdpt.notifyDataSetChanged();
 				return true;
-			}
-	    else finish();
-	    }
+			} else
+				finish();
+		}
 
-	    return super.onKeyDown(keyCode, event);
+		return super.onKeyDown(keyCode, event);
 	}
 }
-
--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -13,53 +13,52 @@ import android.widget.SearchView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
-        
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        return true;
-    }
-    
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
-    	//Search button was clicked in action bar
-    	case R.id.action_search:
-    		//openSearch();
-    	}
-    	return false;
-    }
-    
-    
-    //Display contents of the external sdcard
-    public void openExternal(View view) {
-    	Intent intent = new Intent(this, DisplayDirectoryActivity.class);
-    	//Need a method for external card, rather than hard coding
-    	intent.putExtra("currentPath", "/storage/extSdCard");
-    	startActivity(intent);
-    }
-    
-    //Display contents of internal sdcard
-    public void openInternal(View view) {
-    	Intent intent = new Intent(this, DisplayDirectoryActivity.class);
-    	intent.putExtra("currentPath", Environment.getExternalStorageDirectory().getPath());
-    	startActivity(intent);
-    }
-    
-    //Search using the given term
-    public void openSearch() {
-    	
-    }
+		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+		searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		// Search button was clicked in action bar
+		case R.id.action_search:
+			openSearch();
+		}
+		return false;
+	}
+
+	// Display contents of the external sdcard
+	public void openExternal(View view) {
+		Intent intent = new Intent(this, DisplayDirectoryActivity.class);
+		// Need a method for external card, rather than hard coding
+		intent.putExtra("currentPath", "/storage/extSdCard");
+		startActivity(intent);
+	}
+
+	// Display contents of internal sdcard
+	public void openInternal(View view) {
+		Intent intent = new Intent(this, DisplayDirectoryActivity.class);
+		intent.putExtra("currentPath", Environment
+				.getExternalStorageDirectory().getPath());
+		startActivity(intent);
+	}
+
+	// Search using the given term
+	public void openSearch() {
+		Intent intent = new Intent(this, DisplayDirectoryActivity.class);
+		intent.setAction(Intent.ACTION_SEARCH);
+		startActivity(intent);
+	}
 }
-    
-
