6d94b93c6208e70756e6d2538dbfb89fcd29ae6e
abnegate <jake_barnby@hotmail.com>
2013-11-30 04:40:16 +1300

    Added class for async operation - needs completion
    Signed-off-by: abnegate <jake_barnby@hotmail.com>

---
+++ src/com/example/myapp/MoveFiles.java
@@ -0,0 +1,15 @@
+package com.example.myapp;
+
+import java.io.File;
+
+import android.os.AsyncTask;
+
+public class MoveFiles extends AsyncTask<File, Long, Boolean> {
+
+	@Override
+	protected Boolean doInBackground(File... arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
